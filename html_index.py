673c3709542fe62b11481eae78facb2c69b3636e
HuanPc <arcolinux@email.com>
2025-11-05 19:05:14 +0100

    favicon nuevo

--- html_index.py
+++ html_index.py
@@ -100,7 +100,7 @@ def generate_index_html(files):
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>RYM Hispano EstadÃ­sticas</title>
-        <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+        <link rel="icon" type="image/png" href="images/music.png">
         <style>
             * {
                 margin: 0;

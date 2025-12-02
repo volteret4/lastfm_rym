e3e3c716db473972d08e65bb6b772d4f9298315d
HuanPc <arcolinux@email.com>
2025-12-02 22:52:22 +0100

    commented out failing tests

--- tools/group/group_stats_html_generator.py
+++ tools/group/group_stats_html_generator.py
@@ -1134,10 +1134,10 @@ class GroupStatsHTMLGenerator:
     </div>
 
     <script>
-        const groupStats = {{stats_json}};
-        const colors = {{colors_json}};
-        const userIcons = {{user_icons_json}};
-        const periodFolder = "{{period_folder}}";
+        const groupStats = {stats_json};
+        const colors = {colors_json};
+        const userIcons = {user_icons_json};
+        const periodFolder = "{period_folder}";
 
         let currentView = 'data';
         let charts = {{}};
@@ -2400,7 +2400,6 @@ class GroupStatsHTMLGenerator:
             // Usar archivos JSON específicos para scatter charts (NUEVA LÓGICA CORREGIDA)
             console.log('Cargando datos scatter específicos para usuarios seleccionados...');
 
-            const userKey = getUserKey();
             const scatterData = await loadDynamicData('evolution_scatter', userKey);
 
             if (!scatterData) {{
